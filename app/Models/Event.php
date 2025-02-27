41fd271a4032d1395823047f5ad4ce1e48c6a02f
Mo.Araiqat <107495343+mohammad-ariqat@users.noreply.github.com>
2025-02-27 19:55:06 +0300

    Event model realations

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'location', 'start_date', 'end_date', 'user_id'
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function participants(): HasMany
+    {
+        return $this->hasMany(Participant::class);
+    }
+
+    public function materials(): HasMany
+    {
+        return $this->hasMany(Material::class);
+    }
+
+    public function feedback(): HasMany
+    {
+        return $this->hasMany(Feedback::class);
+    }
 }
